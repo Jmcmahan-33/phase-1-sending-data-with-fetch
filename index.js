a7165099ef1377a5d20fe07c1595fe7f3f1e0676
Jared McMahan <jaredm788@gmail.com>
2022-09-06 18:03:47 -0600

    commit tree 3e30f67709c9ff3ba0f8b48e3386e7f0fdd446e2

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+const submitData = function (name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: "Steve",
+            email:"steve@steve.com",
+        })
+    })
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (object) {
+        console.log(object)
+        document.body.innerHTML = object["id"]
+    })
+    .catch(function (error) {
+        alert("oh no")
+        document.body.innerHTML = error.message
+    })
+    
+    }
+
+
+
+
